e25343d8248bf2b0c66f1940d8b50ad3b253f43d
hakolao <okkohakola@gmail.com>
2022-07-19 15:53:09 +0300

    Add egui demo app to make sure everything works

---
+++ examples/demo_app.rs
@@ -0,0 +1,83 @@
+// Copyright (c) 2021 Okko Hakola
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
+
+use egui_winit_vulkano::Gui;
+use vulkano_util::{
+    context::{VulkanoConfig, VulkanoContext},
+    window::{VulkanoWindows, WindowDescriptor},
+};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
+
+pub fn main() {
+    // Winit event loop
+    let event_loop = EventLoop::new();
+    // Vulkano context
+    let context = VulkanoContext::new(VulkanoConfig::default());
+    // Vulkano windows (create one)
+    let mut windows = VulkanoWindows::default();
+    windows.create_window(&event_loop, &context, &WindowDescriptor::default(), |_| {});
+    // Create gui as main render pass (no overlay means it clears the image each frame)
+    let mut gui = {
+        let renderer = windows.get_primary_renderer_mut().unwrap();
+        Gui::new(renderer.surface(), renderer.graphics_queue(), false)
+    };
+    // Display the demo application that ships with egui.
+    let mut demo_app = egui_demo_lib::DemoWindows::default();
+
+    event_loop.run(move |event, _, control_flow| {
+        let renderer = windows.get_primary_renderer_mut().unwrap();
+        match event {
+            Event::WindowEvent { event, window_id }
+                if window_id == renderer.surface().window().id() =>
+            {
+                // Update Egui integration so the UI works!
+                let _pass_events_to_game = !gui.update(&event);
+                match event {
+                    WindowEvent::Resized(_) => {
+                        renderer.resize();
+                    }
+                    WindowEvent::ScaleFactorChanged { .. } => {
+                        renderer.resize();
+                    }
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                    _ => (),
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == window_id => {
+                // Set immediate UI in redraw here
+                gui.immediate_ui(|gui| {
+                    let ctx = gui.context();
+                    demo_app.ui(&ctx);
+                });
+                // Alternatively you could
+                // gui.begin_frame();
+                // let ctx = gui.context();
+                // demo_app.ui(&ctx);
+
+                // Render UI
+                // Acquire swapchain future
+                let before_future = renderer.acquire().unwrap();
+                // Render gui
+                let after_future =
+                    gui.draw_on_image(before_future, renderer.swapchain_image_view());
+                // Present swapchain
+                renderer.present(after_future, true);
+            }
+            Event::MainEventsCleared => {
+                renderer.surface().window().request_redraw();
+            }
+            _ => (),
+        }
+    });
+}
--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,3 +1,4 @@
 cargo run --example wholesome --release
 cargo run --example minimal --release
 cargo run --example subpass --release
+cargo run --example demo_app --release
