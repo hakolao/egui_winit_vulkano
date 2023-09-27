79047a76982a3c9a1e94a85292426501a69019e3
Firestar99 <4696087-Firestar99@users.noreply.gitlab.com>
2023-09-27 19:44:17 +0200

    srgb linear conversion methods are all in range 0-1, to make reasoning easier

--- src/renderer.rs
+++ src/renderer.rs
@@ -759,11 +759,13 @@ layout(push_constant) uniform PushConstants {
 } push_constants;
 
 void main() {
-  gl_Position =
-      vec4(2.0 * position.x / push_constants.screen_size.x - 1.0,
-           2.0 * position.y / push_constants.screen_size.y - 1.0, 0.0, 1.0);
-  v_color = color;
-  v_tex_coords = tex_coords;
+    gl_Position = vec4(
+        2.0 * position.x / push_constants.screen_size.x - 1.0,
+        2.0 * position.y / push_constants.screen_size.y - 1.0,
+        0.0, 1.0
+    );
+    v_color = color;
+    v_tex_coords = tex_coords;
 }"
     }
 }
@@ -787,28 +789,30 @@ layout(push_constant) uniform PushConstants {
     int need_srgb_conv;
 } push_constants;
 
-// 0-255 sRGB  from  0-1 linear
-vec3 srgb_from_linear(vec3 rgb) {
-  bvec3 cutoff = lessThan(rgb, vec3(0.0031308));
-  vec3 lower = rgb * vec3(3294.6);
-  vec3 higher = vec3(269.025) * pow(rgb, vec3(1.0 / 2.4)) - vec3(14.025);
-  return mix(higher, lower, vec3(cutoff));
+// 0-1 sRGB  from  0-1 linear
+vec3 srgb_from_linear(vec3 linear) {
+    bvec3 cutoff = lessThan(linear, vec3(0.0031308));
+    vec3 lower = linear * vec3(12.92);
+    vec3 higher = vec3(1.055) * pow(linear, vec3(1./2.4)) - vec3(0.055);
+    return mix(higher, lower, vec3(cutoff));
 }
 
-vec4 srgba_from_linear(vec4 rgba) {
-  return vec4(srgb_from_linear(rgba.rgb), 255.0 * rgba.a);
+// 0-1 sRGBA  from  0-1 linear
+vec4 srgba_from_linear(vec4 linear) {
+    return vec4(srgb_from_linear(linear.rgb), linear.a);
 }
 
-// 0-1 linear  from  0-255 sRGB
+// 0-1 linear  from  0-1 sRGB
 vec3 linear_from_srgb(vec3 srgb) {
-    bvec3 cutoff = lessThan(srgb, vec3(10.31475));
-    vec3 lower = srgb / vec3(3294.6);
-    vec3 higher = pow((srgb + vec3(14.025)) / vec3(269.025), vec3(2.4));
-    return mix(higher, lower, cutoff);
+    bvec3 cutoff = lessThan(srgb, vec3(0.04045));
+    vec3 lower = srgb / vec3(12.92);
+    vec3 higher = pow((srgb + vec3(0.055) / vec3(1.055)), vec3(2.4));
+    return mix(higher, lower, vec3(cutoff));
 }
 
-vec4 linear_from_srgba(vec4 srgba) {
-    return vec4(linear_from_srgb(srgba.rgb * 255.0), srgba.a);
+// 0-1 linear  from  0-1 sRGB
+vec4 linear_from_srgba(vec4 srgb) {
+    return vec4(linear_from_srgb(srgb.rgb), srgb.a);
 }
 
 void main() {
@@ -816,7 +820,7 @@ void main() {
     // We must convert vertex color to linear AFTER interpolation happened
     vec4 color = linear_from_srgba(v_color);
 
-    f_color = srgba_from_linear(color * texture_color) / 255.0;
+    f_color = srgba_from_linear(color * texture_color);
 }"
     }
 }
