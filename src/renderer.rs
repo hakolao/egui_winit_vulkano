8374800fe2adaf2342700cdb6cace455cf11eb60
Firestar99 <4696087-Firestar99@users.noreply.gitlab.com>
2023-09-27 15:12:42 +0200

    fixed srgb color space vertex interpolation v2

--- src/renderer.rs
+++ src/renderer.rs
@@ -758,24 +758,11 @@ layout(push_constant) uniform PushConstants {
     int need_srgb_conv;
 } push_constants;
 
-// 0-1 linear  from  0-255 sRGB
-vec3 linear_from_srgb(vec3 srgb) {
-    bvec3 cutoff = lessThan(srgb, vec3(10.31475));
-    vec3 lower = srgb / vec3(3294.6);
-    vec3 higher = pow((srgb + vec3(14.025)) / vec3(269.025), vec3(2.4));
-    return mix(higher, lower, cutoff);
-}
-
-vec4 linear_from_srgba(vec4 srgba) {
-    return vec4(linear_from_srgb(srgba.rgb * 255.0), srgba.a);
-}
-
 void main() {
   gl_Position =
       vec4(2.0 * position.x / push_constants.screen_size.x - 1.0,
            2.0 * position.y / push_constants.screen_size.y - 1.0, 0.0, 1.0);
-  // We must convert vertex color to linear
-  v_color = linear_from_srgba(color);
+  v_color = color;
   v_tex_coords = tex_coords;
 }"
     }
@@ -826,11 +813,13 @@ vec4 linear_from_srgba(vec4 srgba) {
 
 void main() {
     vec4 texture_color = texture(font_texture, v_tex_coords);
+    // We must convert vertex color to linear AFTER interpolation happened
+    vec4 color = linear_from_srgba(v_color);
 
     if (push_constants.need_srgb_conv == 0) {
-        f_color = v_color * texture_color;
+        f_color = color * texture_color;
     } else {
-        f_color = srgba_from_linear(v_color * texture_color) / 255.0;
+        f_color = srgba_from_linear(color * texture_color) / 255.0;
         f_color.a = pow(f_color.a, 1.6);
     }
 }"
