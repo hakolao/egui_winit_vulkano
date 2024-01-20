1755d90cf0b6fbd44dfe9627721aa2983d993e93
let <timbukreew624@gmail.com>
2024-01-20 09:16:17 +0100

    clippy + add +nightly in run_checks.sh so that script works for everyone

--- examples/wholesome/frame_system.rs
+++ examples/wholesome/frame_system.rs
@@ -119,13 +119,10 @@ impl FrameSystem {
             )
             .unwrap();
         }
-        let framebuffer = Framebuffer::new(
-            self.render_pass.clone(),
-            FramebufferCreateInfo {
-                attachments: vec![final_image, self.depth_buffer.clone()],
-                ..Default::default()
-            },
-        )
+        let framebuffer = Framebuffer::new(self.render_pass.clone(), FramebufferCreateInfo {
+            attachments: vec![final_image, self.depth_buffer.clone()],
+            ..Default::default()
+        })
         .unwrap();
         let mut command_buffer_builder = RecordingCommandBuffer::new(
             self.allocators.command_buffers.clone(),
